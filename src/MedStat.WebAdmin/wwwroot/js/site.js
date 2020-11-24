52ee1a9d82d4db21986521281e13902d2a65a433
Vladimir Siarov <vsyarov@hotmail.com>
2020-11-24 20:54:51 +0300

    added Site Nav (aside) panel.
    # added Expand & Collapse buttons

--- src/MedStat.WebAdmin/wwwroot/js/site.js
+++ src/MedStat.WebAdmin/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Site Nav panel
+$('#sitenav_header_btnCollapse').click(function() {
+	$(document.body).removeClass('ms-sitenav-expanded');
+});
+$('#sitenav_header_btnExpand').click(function () {
+	$(document.body).addClass('ms-sitenav-expanded');
+});
