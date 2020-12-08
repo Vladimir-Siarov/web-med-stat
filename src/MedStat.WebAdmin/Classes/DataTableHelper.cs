60da812616b9c01b4c5e637d5619bd05c63da100
Vladimir Siarov <vsyarov@hotmail.com>
2020-12-08 17:42:11 +0300

    Added initial localization for DataTables.net
    #Added light version of Bootstrap grid
    #Improved some styles

---
+++ src/MedStat.WebAdmin/Classes/DataTableHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace MedStat.WebAdmin.Classes
+{
+	public static class DataTableHelper
+	{
+		private const string DataTablePageFlag = "DATA_TABLE_PAGE";
+
+
+		public static void SetDataTablePageFlag(this ViewDataDictionary viewData)
+		{
+			viewData[DataTablePageFlag] = true;
+		}
+
+		public static bool IsDataTablePage(this ViewDataDictionary viewData)
+		{
+			return 
+				viewData.ContainsKey(DataTablePageFlag) && (bool)viewData[DataTablePageFlag];
+		}
+
+	}
+}
--- src/MedStat.WebAdmin/wwwroot/js/pages/companyListPage.js
+++ src/MedStat.WebAdmin/wwwroot/js/pages/companyListPage.js
@@ -1,15 +1,34 @@
 ﻿$(function () {
 
 	$('#tblCompanies').DataTable({
-		"processing": true,
-		"serverSide": true,
-		"lengthMenu": [2, 10, 25, 50],
+		processing: true,
+		serverSide: true,
+
+		lengthMenu: [2, 10, 25, 50],
+		pagingType: "first_last_numbers",
 		//"ajax": "@(Url.PageLink("Index", "OnGetCompanyListAsync"))",
-		"columns": [
+
+		columns: [
 			{ "data": "id" },
 			{ "data": "name" },
 			{ "data": "description" }
-		]
+		],
+
+		language: {
+			processing: 'загрузка...', // "Processing..."
+			loadingRecords: 'загрузка...', // "Loading..."
+
+			lengthMenu: "Показывать _MENU_ записей", // "Show _MENU_ entries"
+			search: "Поиск:", // "Search:"
+
+			info: "Показано с _START_ по _END_ из _TOTAL_ записей", // "Showing _START_ to _END_ of _TOTAL_ entries"
+			paginate: {
+				first: "Первая",
+				previous: "Предыдущая",
+				next: "Следующая",
+				last: "Последняя"
+			}
+		}
 	});
 
 });
